dab3d838d746a30676fee3e8799e0d85fe7aaa8c
chandrashekarreddy1331 <chandrashekarreddy1331@gmail.com>
2023-11-27 11:23:00 -0600

    types of python add to git hub

---
+++ types.py
@@ -0,0 +1,32 @@
+b = 6.0
+a = int(b)
+print(a)
+print(type(b))
+c=9+9j
+print(type(c))
+c= complex(5,6)
+print(c)
+
+chandu=[1,2,3,4,5]
+print(chandu)
+
+chandu_set={2,2,3,3,5,6,7,89}
+print(chandu_set)
+
+print(chandu_set)
+
+chandu_tuple=(2,3,4,5,5,5,9)
+print(chandu_tuple)
+chandu[0]=4
+print((chandu))
+
+a=list(range(0,11,2))
+print(a)
+
+harsh = {"kiran": "pulsar", "chandu": "shine", "sumith": "passion pro"}
+
+print(harsh['chandu'])
+print(harsh.get("sumith"))
+print(harsh.keys())
+print(harsh.values())
+
